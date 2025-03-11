2cf70e73b369fc0b47fa5e7d241dbe96bc158f31
jan438 <jan438@users.noreply.github.com>
2025-03-11 10:09:22 +0100

    foto added

--- Olieslager.py
+++ Olieslager.py
@@ -29,13 +29,14 @@ middlehormargin = 15
 bottommargin = 5
 
 class Kamer:
-    def __init__(self, nummer, pad, zijde, bewoner, initialen, naam):
+    def __init__(self, nummer, pad, zijde, bewoner, initialen, naam, foto):
         self.nummer = nummer
         self.pad = pad
         self.zijde = zijde
         self.bewoner = bewoner
         self.initialen = initialen
         self.naam = naam
+        self.foto = foto
 
 def processcsv(csvfile):
     with open(file_to_open, 'r') as file:
@@ -58,7 +59,7 @@ def fillKamerReport(count):
         d.add(Rect(leftmargin + rectwidth + middlehormargin, bottommargin + (i * rectheight), rectwidth, rectheight, fillColor = colors.yellow))
    
     for i in range(len(kamers)):
-        print(kamers[i].bewoner, kamers[i].initialen, kamers[i].naam)
+        print(kamers[i].bewoner, kamers[i].initialen, kamers[i].naam, kamers[i].foto)
 
     d.add(String(100, 155, '259', fontSize = 20, fillColor = colors.blue))
     d.add(String(100, 130, 'Dick', fontSize = 15, fillColor = colors.red))
@@ -87,7 +88,8 @@ for i in range(len(kamerdata)):
     bewoner = kamerdata[i][3]
     initialen = kamerdata[i][4]
     naam = kamerdata[i][5]
-    kamers.append(Kamer(nummer, pad, zijde, bewoner, initialen, naam))
+    foto = kamerdata[i][6]
+    kamers.append(Kamer(nummer, pad, zijde, bewoner, initialen, naam, foto))
 
 pdfmetrics.registerFont(TTFont('Ubuntu', 'Ubuntu-Regular.ttf'))
 pdfmetrics.registerFont(TTFont('UbuntuBold', 'Ubuntu-Bold.ttf'))
