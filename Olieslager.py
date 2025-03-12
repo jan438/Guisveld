8743098d762197e42dd7a6b33c6148ad0cec03fb
jan438 <jan438@users.noreply.github.com>
2025-03-12 10:04:11 +0100

    code changed

--- Olieslager.py
+++ Olieslager.py
@@ -69,8 +69,7 @@ def fillKamerReport(count):
     for i in range(len(kamers)):
         print(kamers[i].bewoner, kamers[i].initialen, kamers[i].naam, kamers[i].foto)
     d.add(String(leftmargin, bottommargin + topmargin, "Wegzijde", fontSize = 20, fillColor = colors.purple))
-    d.add(String(leftmargin + middlehormargin + rectwidth, bottommargin + topmargin, "Tuinzijde", fontSize = 20, fillColor = colors.purple))
-    d.add(String(leftmargin + 2*rectwidth+middlehormargin + middlehorseparator, bottommargin + topmargin , "Tuinzijde", fontSize = 20, fillColor = colors.purple))
+    d.add(String(leftmargin + middlehormargin + 1.75*rectwidth, bottommargin + topmargin, "Tuinzijde", fontSize = 20, fillColor = colors.purple))
     d.add(String(leftmargin + 2*rectwidth + 2*middlehormargin + middlehorseparator + rectwidth, bottommargin + topmargin, "Wegzijde", fontSize = 20, fillColor = colors.purple))
     d.add(String(leftmargin + 2.5 + middlehormargin + rectwidth, bottommargin + 8 + (7 * rectheight), "Hazenpad", fontSize = 20, fillColor = colors.purple))
     d.add(String(leftmargin + 2.5 + 3*rectwidth + 2*middlehormargin + middlehorseparator, bottommargin + 8 + (7 * rectheight), "Boerenpad", fontSize = 20, fillColor = colors.purple))
