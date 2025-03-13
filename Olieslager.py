75b6bcaf5a53438fa2dcf9b20a8a9fac2fb8cf1a
jan438 <jan438@users.noreply.github.com>
2025-03-13 10:05:47 +0100

    add initialen

--- Olieslager.py
+++ Olieslager.py
@@ -44,7 +44,7 @@ class Kamer:
 def lookuproomposition(number):
     roomposition = [[] for _ in range(300)]
     roomposition[258] = [12, 14]
-    roomposition[259] = [100, 13]
+    roomposition[259] = [100, 400]
     roomposition[260] = [16, 15]
     return roomposition[int(number)]
 
@@ -89,7 +89,7 @@ def fillKamerReport(count):
         if kamers[i].pad == "Middelpunt":
             print("Middelpunt", roompos[0], roompos[1])
             d.add(String(roompos[0], roompos[1], kamers[i].nummer, fontSize = 20, fillColor = colors.blue))
-            #d.add(String(82.5, bottommargin + 25 + (i * rectheight), kamers[i].initialen, fontSize = 10, fillColor = colors.red))
+            d.add(String(roompos[0] - 17.5, roompos[1], kamers[i].initialen, fontSize = 10, fillColor = colors.red))
             #d.add(String(82.5, bottommargin + 10 + (i * rectheight), kamers[i].naam, fontSize = 10, fillColor = colors.red))
             #d.add(Image(path = "Foto/" + kamers[i].foto, width = 75, height = 95, x = leftmargin + 2.5, y = 7.5 + (i * rectheight)))
     return
