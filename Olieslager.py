bf59c5d696e75c69c9ea98e77408b59df82ff44f
jan438 <jan438@users.noreply.github.com>
2025-03-14 18:30:30 +0100

    error corrected

--- Olieslager.py
+++ Olieslager.py
@@ -49,7 +49,7 @@ def lookuproomposition(number):
     # 244     x = 155  y = 600
     # 250     x = 155  y = 8
     # 251     x = 7.5  y = 8
-    # 258     x = 7.5  y = 608
+    # 258     x = 7.5  y = 708
     # 260     x = 450  y = 708
     # 267     x = 450  y = 8
     # 268     x = 305  y = 8
@@ -57,7 +57,7 @@ def lookuproomposition(number):
     roomposition[244] = [155, 608]
     roomposition[250] = [155, 8]
     roomposition[251] = [7.5, 8]
-    roomposition[258] = [7.5, 608]
+    roomposition[258] = [7.5, 708]
     roomposition[259] = [300, 708]
     roomposition[260] = [450, 708]
     roomposition[267] = [450, 8]
