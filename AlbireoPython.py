d62b30631ad1c4fc69c3eb56721815014f0a4d14
jan438 <jan438@users.noreply.github.com>
2026-06-11 14:19:24 +0200

    added

--- AlbireoPython.py
+++ AlbireoPython.py
@@ -49,16 +49,17 @@ def albireo(c, x, y):
     albireo_width = 330
     middle_x = x + albireo_width / 2
     middle_beziers_x = x + 15
+    middle_beziers_y = y + 15
     c.setFillColor(HexColor('#c7c7c7'))
     c.rect(x, y, albireo_width, 600, stroke=0, fill=1)
-    renderPDF.draw(scaleSVG("SVG/arc_330_30degouter.svg", 1.0), c, middle_beziers_x, y + 20)
-    renderPDF.draw(scaleSVG("SVG/arc_330_30deg.svg", 1.0), c, middle_beziers_x, y + 20)
-    renderPDF.draw(scaleSVG("SVG/arc_60_120degouter.svg", 1.0), c,middle_beziers_x, y + 20)
-    renderPDF.draw(scaleSVG("SVG/arc_60_120deg.svg", 1.0), c, middle_beziers_x, y + 20)
-    renderPDF.draw(scaleSVG("SVG/arc_150_210degouter.svg", 1.0), c, middle_beziers_x, y + 20)
-    renderPDF.draw(scaleSVG("SVG/arc_150_210deg.svg", 1.0), c, middle_beziers_x, y + 20)
-    renderPDF.draw(scaleSVG("SVG/arc_240_300degouter.svg", 1.0), c, middle_beziers_x, y + 20)
-    renderPDF.draw(scaleSVG("SVG/arc_240_300deg.svg", 1.0), c, middle_beziers_x, y + 20)
+    renderPDF.draw(scaleSVG("SVG/arc_330_30degouter.svg", 1.0), c, middle_beziers_x, middle_beziers_y)
+    renderPDF.draw(scaleSVG("SVG/arc_330_30deg.svg", 1.0), c, middle_beziers_x, middle_beziers_y)
+    renderPDF.draw(scaleSVG("SVG/arc_60_120degouter.svg", 1.0), c,middle_beziers_x, middle_beziers_y)
+    renderPDF.draw(scaleSVG("SVG/arc_60_120deg.svg", 1.0), c, middle_beziers_x, middle_beziers_y)
+    renderPDF.draw(scaleSVG("SVG/arc_150_210degouter.svg", 1.0), c, middle_beziers_x, middle_beziers_y)
+    renderPDF.draw(scaleSVG("SVG/arc_150_210deg.svg", 1.0), c, middle_beziers_x, middle_beziers_y)
+    renderPDF.draw(scaleSVG("SVG/arc_240_300degouter.svg", 1.0), c, middle_beziers_x, middle_beziers_y)
+    renderPDF.draw(scaleSVG("SVG/arc_240_300deg.svg", 1.0), c, middle_beziers_x, middle_beziers_y)
     c.setFillColor(HexColor('#ff0000'))
     c.circle(middle_x, alarmbutton_y, alarmbutton_r, stroke=0, fill=1)
     c.setFillColor(HexColor('#ffffff'))
